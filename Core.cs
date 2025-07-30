a8ba0b0c05fcc056a81b6c0e1889ce448dd51092
pierre <pierre.santerre2@outlook.com>
2025-07-29 17:32:40 -0700

    Other core constructor

--- Core.cs
+++ Core.cs
@@ -33,17 +33,62 @@ namespace sudoku
         public readonly UI ui;
         public Cell[] cells;
 
+        /// <summary>
+        /// Class used to be able to pass a void method as a Func
+        /// </summary>
+        public class VoidClass
+        {
+            /// <summary>
+            /// For showing in the debugger
+            /// </summary>
+            /// <returns></returns>
+            public override string ToString()
+            {
+                return "VoidClass";
+            }
+        }
+
+        /// <summary>
+        /// Return a string of length * the same character
+        /// </summary>
+        /// <param name="character"></param>
+        /// <param name="length"></param>
+        /// <returns>The string of length of the same character</returns>
         public static string RepeatChar(char character, int length)
         {
             string result = "".PadLeft(length, character);
             return result;
         }
 
-        public static string ArrayToString<E>(E[] elememts) where E : class
+        public delegate E ExceptionCallback<E>(Exception exception);
+
+        public static E? BreakWithDebugger<E>(Func<E?> func, ExceptionCallback<E?> exceptionCallback)
+        {
+            E? result = default(E);
+            if (Debugger.IsAttached)
+            {
+                result = func();
+            }
+            else
+            {
+                try
+                {
+                    result = func();
+                }
+                catch (Exception exception)
+                {
+                    result = exceptionCallback(exception);
+                }
+            }
+
+            return result;
+        }
+
+        public static string IEnumerableToString<E>(IEnumerable<E> elements) where E : class
         {
             string result = "[";
             bool first = true;
-            foreach (E e in elememts)
+            foreach (E e in elements)
             {
                 if (!first)
                 {
@@ -283,27 +328,8 @@ namespace sudoku
             public override string ToString()
             {
                 string result = $"{groupType.ToString()}({ordinal})=";
-                Mask[] masks = cells.Select(x => x.mask).ToArray();
-                result += ArrayToString(masks);
-                return result;
-            }
-        }
-
-        public class InitialCellDigit
-        {
-            public int x;
-            public int y;
-            public char display;
-            public InitialCellDigit(int x, int y, char display)
-            {
-                this.x = x;
-                this.y = y;
-                this.display = display;
-            }
-
-            public override string ToString()
-            {
-                string result = $"{x},{y} = {display}";
+                IEnumerable<Mask> masks = cells.Select(x => x.mask);
+                result += IEnumerableToString(masks);
                 return result;
             }
         }
@@ -332,48 +358,134 @@ namespace sudoku
             }
         }
 
-        private void SolveWithExceptions(InitialCellDigit[] initialCellDigits)
+        public class InternalInitialCellDigit
         {
-            foreach (InitialCellDigit initialCellDigit in initialCellDigits)
+            public int ordinal;
+            public Digit digit;
+
+            public InternalInitialCellDigit(Core core, int cellOrdinal, char display)
             {
-                int x = initialCellDigit.x - 1;
-                if (x < 0 || x >= numDigits)
-                {
-                    throw new Exception($"Invalid initial cell: {initialCellDigit}");
-                }
-                int y = initialCellDigit.y - 1;
-                if (y < 0 || y >= numDigits)
-                {
-                    throw new Exception($"Invalid initial cell: {initialCellDigit}");
-                }
-                int cellOrdinal = x + (y * numDigits);
-                int digitOrdinal = DisplayToOrdinal(initialCellDigit.display);
-                Digit digit = digits[digitOrdinal];
-                cells[cellOrdinal].SetCell(digit);
+                ordinal = cellOrdinal;
+                int digitOrdinal = core.DisplayToOrdinal(display);
+                Digit digit = core.digits[digitOrdinal];
+                this.digit = digit;
             }
-            ui.Log("After initial cells", ConsoleColor.Green);
-            ui.FullUI(cells);
+
+            public override string ToString()
+            {
+                string result = $"{ordinal}={digit}";
+                return result;
+            }
         }
 
-        public void Solve(InitialCellDigit[] initialCellDigits)
+        public class InitialCellDigit
+        {
+            public int x;
+            public int y;
+            public char display;
+
+            public InternalInitialCellDigit ToInternalInitialCellDigit(Core core)
+            {
+                x = x - 1;
+                if (x < 0 || x >= core.numDigits)
+                {
+                    throw new Exception($"Invalid initial cell: {this}");
+                }
+                y = y - 1;
+                if (y < 0 || y >= core.numDigits)
+                {
+                    throw new Exception($"Invalid initial cell: {this}");
+                }
+                int cellOrdinal = x + (y * core.numDigits);
+                InternalInitialCellDigit result = new InternalInitialCellDigit(core, cellOrdinal, display);
+                return result;
+            }
+
+            public InitialCellDigit(int x, int y, char display)
+            {
+                this.x = x;
+                this.y = y;
+                this.display = display;
+            }
+
+            public override string ToString()
+            {
+                string result = $"{x},{y} = {display}";
+                return result;
+            }
+        }
+
+        internal InternalInitialCellDigit[] StringsToInternalInitialCellDigits(Core core, string[] lines)
+        {
+            List<InternalInitialCellDigit> internalInitialCellDigits = new List<InternalInitialCellDigit>();
+            if (lines.Length != numDigits)
+            {
+                throw new Exception($"We must have {numDigits} initialization strings, not {lines.Length}");
+            }
+
+            for (int i = 0; i < numDigits; i++)
+            {
+                string line = lines[i];
+                if (line.Length != numDigits)
+                {
+                    throw new Exception($"Initialization strings must have length {numDigits}, not {line.Length} on line {i}: {line}");
+                }
+                for (int j = 0; j < numDigits; j++)
+                {
+                    if (line[j] != ' ')
+                    {
+                        internalInitialCellDigits.Add(new InternalInitialCellDigit(core, (i * numDigits) + j, line[j]));
+                    }
+                }
+            }
+
+            InternalInitialCellDigit[] result = internalInitialCellDigits.ToArray();
+            return result;
+        }
+
+        private bool SolveWithExceptions(InternalInitialCellDigit[] internalInitialCellDigits)
         {
             ui.Log("Starting", ConsoleColor.Green);
             ui.FullUI(cells);
-            if (Debugger.IsAttached)
-            {
-                SolveWithExceptions(initialCellDigits);
+            foreach (InternalInitialCellDigit internalInitialCellDigit in internalInitialCellDigits)
+            { 
+                cells[internalInitialCellDigit.ordinal].SetCell(internalInitialCellDigit.digit);
             }
-            else
-            {
-                try
-                {
-                    SolveWithExceptions(initialCellDigits);
-                }
-                catch (Exception exception)
-                {
-                    ui.Log($"Exception: {exception.Message}", ConsoleColor.DarkRed);
-                }
+            ui.Log("After initial cells", ConsoleColor.Green);
+            ui.FullUI(cells);
+            return true;
+        }
+
+        public bool Solve(InitialCellDigit[] initialCellDigits)
+        {
+            bool result = BreakWithDebugger(() => {
+                InternalInitialCellDigit[] internalInitialCellDigits = initialCellDigits.Select(x => x.ToInternalInitialCellDigit(this)).ToArray();
+                bool exceptionResult = SolveWithExceptions(internalInitialCellDigits);
+                return exceptionResult;
             }
+            , (exception) =>
+            {
+                ui.Log($"Exception: {exception.Message}", ConsoleColor.DarkRed);
+                return false;
+            });
+
+            return result;
+        }
+
+        public bool Solve(string[] lines)
+        {
+            bool result = BreakWithDebugger(() => {
+                InternalInitialCellDigit[] internalInitialCellDigits = StringsToInternalInitialCellDigits(this, lines);
+                bool exceptionResult = SolveWithExceptions(internalInitialCellDigits);
+                return exceptionResult;
+            }
+            , (exception) =>
+            {
+                ui.Log($"Exception: {exception.Message}", ConsoleColor.DarkRed);
+                return false;
+            });
+
+            return result;
         }
 
         public Core(int boxWidthParam, int boxHeightParam)
--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         public static int Main(string[] args)
         {
             // Seattle Times Comics 7/27/2025
-            Core core = new Core(3, 2);
+            Core core;
+            
+            core = new Core(3, 2);
             Core.InitialCellDigit[] initialCellDigits =
             [
                 new Core.InitialCellDigit(1, 1, '6'),
@@ -19,8 +21,19 @@
                 new Core.InitialCellDigit(1, 6, '1'),
                 new Core.InitialCellDigit(6, 6, '3'),
             ];
-
             core.Solve(initialCellDigits);
+
+            core = new Core(3, 2);
+            core.Solve(
+                [
+                    "6    5",
+                    "  32  ",
+                    "  1   ",
+                    "   4  ",
+                    "  25  ",
+                    "1    3",
+                ]);
+
             return 0;
         }
     }
